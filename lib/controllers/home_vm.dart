8b3fc5497cf3e0a23d6de0a63ffd1dc2267bd951
Gözde Aydın <gozdearzu.aydin@spexco.com>
2023-07-05 22:35:43 +0300

    play button

--- lib/controllers/home_vm.dart
+++ lib/controllers/home_vm.dart
@@ -1,21 +1,28 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
+import 'package:freechoice_music/pages/home.dart';
 import 'package:get/get.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 
 import '../utilities/enums.dart';
 
 //TODO player icon durumu göre değişecek, gerekirse buton olarak ayır state e göre
-//tODO vcs
 
 class HomeVM extends GetxController {
   SwiperController swiperController = SwiperController();
   AudioPlayer player = AudioPlayer();
   RxList<SongModel> files = <SongModel>[].obs;
-  Rx<IconData> playIcon = Rx<IconData>(Icons.play_arrow);
+  Rx<Widget> playButton = Rx<Widget>(const SizedBox());
   RxInt currentIndex = 0.obs;
   final OnAudioQuery _audioQuery = OnAudioQuery();
+
+
+  @override
+  void onInit() {
+    playButton.value = const HomePage().playButton(this);
+    super.onInit();
+  }
 
   @override
   Future<void> onReady() async {
@@ -36,39 +43,21 @@ class HomeVM extends GetxController {
     print(files.length);
   }
 
-  Future<void> checkPlayingStatus() async {
-    if (player.state == PlayerState.playing) {
-      await player.pause();
-      playIcon.value = Icons.play_arrow;
-      return;
-    } else if (player.state == PlayerState.paused) {
-      await player.resume();
-      playIcon.value = Icons.pause;
-      return;
-    } else {
-      files.shuffle();
-      await player.play(DeviceFileSource(files[currentIndex.value].data));
-      return;
-    }
-  }
-
   testplay(PlayType p, {int? i}) async {
     try {
+      //files.shuffle();
       if (p == PlayType.next) {
-        //files.shuffle();
+        playButton.value = const HomePage().pauseButton(this);
         await player
             .play(DeviceFileSource(files[(i ?? currentIndex.value) + 1].data));
-        playIcon.value = Icons.pause;
         swiperController.next();
       } else if (p == PlayType.previous) {
-        //files.shuffle();
+        playButton.value = const HomePage().pauseButton(this);
         await player
             .play(DeviceFileSource(files[(i ?? currentIndex.value) - 1].data));
-        playIcon.value = Icons.pause;
         swiperController.previous();
       } else {
-        //files.shuffle();
-        playIcon.value = Icons.pause;
+        playButton.value = const HomePage().pauseButton(this);
         await player
             .play(DeviceFileSource(files[i ?? currentIndex.value].data));
       }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,8 +5,6 @@ import 'package:freechoice_music/utilities/extensions/sizer.dart';
 import 'package:freechoice_music/utilities/themes/light_theme.dart';
 import 'package:freechoice_music/utilities/widgets/custom_widget/background_grad.dart';
 import 'package:get/get.dart';
-import 'package:just_audio/just_audio.dart';
-
 import '../utilities/enums.dart';
 
 class HomePage extends StatelessWidget {
@@ -15,60 +13,57 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var vm = Get.put(HomeVM());
-    return GestureDetector(
-      onDoubleTap: () => vm.player.pause(),
-      child: Scaffold(
-          body: Stack(
-        alignment: Alignment.topCenter,
-        children: [
-          const BackgroundGrad(colors: [Color(0xff6109AF), Color(0xff130023)]),
-          Column(
-            children: [
-              SizedBox(
-                //nereden çalındıgı yazsın, çalma listesi mesela veya klasör
-                height: 15.h, //appbar olacak
-              ),
-              Obx(
-                () => SizedBox(
-                  height: 50.h,
-                  child: Swiper(
-                    onIndexChanged: (i) {
-                      if (vm.files.isNotEmpty) {
-                        vm.currentIndex.value = i;
-                        //vm.checkPlayingStatus();
-                        vm.testplay(i: i, PlayType.current);
-                      }
-                    },
-                    loop: false,
-                    controller: vm.swiperController,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Container(
-                        padding: EdgeInsets.all(1.h),
-                        decoration: const BoxDecoration(
-                            color: Colors.white,
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(20))),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Text(vm.files[index].title),
-                            Text(vm.files[index].artist ?? ""),
-                          ],
-                        ),
-                      );
-                    },
-                    itemCount: vm.files.length,
-                    viewportFraction: 0.8,
-                    scale: 0.9,
-                  ),
+    return Scaffold(
+        body: Stack(
+      alignment: Alignment.topCenter,
+      children: [
+        const BackgroundGrad(colors: [Color(0xff6109AF), Color(0xff130023)]),
+        Column(
+          children: [
+            SizedBox(
+              //nereden çalındıgı yazsın, çalma listesi mesela veya klasör
+              height: 15.h, //appbar olacak
+            ),
+            Obx(
+              () => SizedBox(
+                height: 50.h,
+                child: Swiper(
+                  onIndexChanged: (i) {
+                    if (vm.files.isNotEmpty) {
+                      vm.currentIndex.value = i;
+                      //vm.checkPlayingStatus();
+                      vm.testplay(i: i, PlayType.current);
+                    }
+                  },
+                  loop: false,
+                  controller: vm.swiperController,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      padding: EdgeInsets.all(1.h),
+                      decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(20))),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(vm.files[index].title),
+                          Text(vm.files[index].artist ?? ""),
+                        ],
+                      ),
+                    );
+                  },
+                  itemCount: vm.files.length,
+                  viewportFraction: 0.8,
+                  scale: 0.9,
                 ),
-              )
-            ],
-          ),
-          Align(alignment: Alignment.bottomCenter, child: playArea(vm))
-        ],
-      )),
-    );
+              ),
+            )
+          ],
+        ),
+        Align(alignment: Alignment.bottomCenter, child: playArea(vm))
+      ],
+    ));
   }
 
   Container playArea(HomeVM vm) {
@@ -85,10 +80,7 @@ class HomePage extends StatelessWidget {
           //customIcon(Icons.shuffle, size: 20.sp),
           customIcon(Icons.skip_previous_rounded,
               onPressed: () => vm.testplay(PlayType.previous)),
-          Obx(() => customIcon(vm.playIcon.value, onPressed: () {
-               // vm.checkPlayingStatus();
-                vm.testplay(PlayType.current);
-              })),
+          Obx(() => vm.playButton.value),
           customIcon(Icons.skip_next,
               onPressed: () => vm.testplay(PlayType.next)),
           //  customIcon(Icons.loop, size: 20.sp)
@@ -97,6 +89,34 @@ class HomePage extends StatelessWidget {
     );
   }
 
+  Flexible playButton(HomeVM vm) {
+    return Flexible(
+        child: IconButton(
+            icon: Icon(
+              Icons.play_arrow,
+              color: LightTheme().darkerIconColor?.withOpacity(0.8),
+              size: 28.sp,
+            ),
+            onPressed: () {
+              vm.testplay(PlayType.current);
+              vm.playButton.value= pauseButton(vm);
+            }));
+  }
+
+  Flexible pauseButton(HomeVM vm) {
+    return Flexible(
+        child: IconButton(
+            icon: Icon(
+              Icons.pause,
+              color: LightTheme().darkerIconColor?.withOpacity(0.8),
+              size: 28.sp,
+            ),
+            onPressed: () {
+              vm.player.pause();
+              vm.playButton.value= playButton(vm);
+
+            }));
+  }
 
   //iconlar değişecek
   Flexible customIcon(IconData iconData,
